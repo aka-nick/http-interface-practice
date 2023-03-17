f40c71fcec8593e9edb0ceba31ca993b59ffa543
aka-nick <bechalrang@gmail.com>
2023-03-17 09:31:31 +0900

    프로젝트 초기화

---
+++ src/test/java/springclient/apicall/ApicallApplicationTests.java
@@ -0,0 +1,13 @@
+package springclient.apicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
