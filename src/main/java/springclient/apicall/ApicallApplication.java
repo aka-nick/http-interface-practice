485d25689aeefae5efa442a7d40203b2575c1c7f
aka-nick <bechalrang@gmail.com>
2023-03-17 10:10:16 +0900

    WebClient로 api call

--- src/main/java/springclient/apicall/ApicallApplication.java
+++ src/main/java/springclient/apicall/ApicallApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ApicallApplication {
@@ -15,16 +16,29 @@ public class ApicallApplication {
 	@Bean
 	ApplicationRunner applicationRunner() {
 		return args -> {
+			// RestTemplate 예제
 			RestTemplate rt = new RestTemplate();
 			Map<String, Map<String, Object>> rtResponse = rt.getForObject(DESTINATION, Map.class);
 
-			System.out.println("==========");
-			System.out.println(rtResponse);
-			System.out.println(rtResponse.get("base_code"));
-			System.out.println(rtResponse.get("rates").get("KRW"));
+			printResponse(rtResponse, "restTemplate");
+
+			// WebClient 예제
+			WebClient wc = WebClient.create();
+			Map<String, Map<String, Object>> wcResponse = wc.get().uri(DESTINATION).retrieve().bodyToMono(Map.class).block();
+
+			printResponse(wcResponse, "webClient");
 
 
 		};
+	}
+
+	private void printResponse(Map<String, Map<String, Object>> response, String client) {
+		System.out.println();
+		System.out.println("==========" + client + "==========");
+//		System.out.println(response);
+//		System.out.println(response.get("base_code"));
+		System.out.println("달러 대비 원화 : " + response.get("rates").get("KRW"));
+		System.out.println("==========================");
 	}
 
 	public static void main(String[] args) {
