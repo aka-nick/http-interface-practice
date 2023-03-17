647cdf5847f855b2224c6cb58c81fd43570df3e1
aka-nick <bechalrang@gmail.com>
2023-03-17 09:58:23 +0900

    RestTemplate로 api call

--- src/main/java/springclient/apicall/ApicallApplication.java
+++ src/main/java/springclient/apicall/ApicallApplication.java
@@ -1,10 +1,31 @@
 package springclient.apicall;
 
+import java.util.Map;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApicallApplication {
+
+	private static final String DESTINATION = "https://open.er-api.com/v6/latest";
+
+	@Bean
+	ApplicationRunner applicationRunner() {
+		return args -> {
+			RestTemplate rt = new RestTemplate();
+			Map<String, Map<String, Object>> rtResponse = rt.getForObject(DESTINATION, Map.class);
+
+			System.out.println("==========");
+			System.out.println(rtResponse);
+			System.out.println(rtResponse.get("base_code"));
+			System.out.println(rtResponse.get("rates").get("KRW"));
+
+
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApicallApplication.class, args);
